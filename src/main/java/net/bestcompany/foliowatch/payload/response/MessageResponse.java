04011c6ecb5b1e131169420dcd0ee8366bb41688
tropicbliss <tropicbliss@protonmail.com>
2023-09-24 00:49:13 +0800

    More lombok

--- src/main/java/net/bestcompany/foliowatch/payload/response/MessageResponse.java
+++ src/main/java/net/bestcompany/foliowatch/payload/response/MessageResponse.java
@@ -1,17 +1,12 @@
 package net.bestcompany.foliowatch.payload.response;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@RequiredArgsConstructor
 public class MessageResponse {
-    private String message;
-
-    public MessageResponse(String message) {
-        this.message = message;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
+    private final String message;
 }
