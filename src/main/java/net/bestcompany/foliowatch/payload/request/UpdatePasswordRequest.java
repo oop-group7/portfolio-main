42d2c1f7258297629e9ea65f8a4e69b1adf5d306
tropicbliss <tropicbliss@protonmail.com>
2023-10-02 22:51:11 +0800

    Update openapi docs

--- src/main/java/net/bestcompany/foliowatch/payload/request/UpdatePasswordRequest.java
+++ src/main/java/net/bestcompany/foliowatch/payload/request/UpdatePasswordRequest.java
@@ -1,18 +1,21 @@
 package net.bestcompany.foliowatch.payload.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
-import net.bestcompany.foliowatch.validators.Password;
 
 @Getter
 @Setter
 public class UpdatePasswordRequest {
-    @Password
     @NotBlank
+    @Size(min = 8, max = 25)
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\\\d)(?=.*[@$!%*?&])[A-Za-z\\\\d@$!%*?&]{8,25}$", message = "Please ensure that your password is between 8-25 characters, has at least a symbol, a numeric character, and an upper and lowercase letter.")
     private String oldPassword;
 
-    @Password
     @NotBlank
+    @Size(min = 8, max = 25)
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\\\d)(?=.*[@$!%*?&])[A-Za-z\\\\d@$!%*?&]{8,25}$", message = "Please ensure that your password is between 8-25 characters, has at least a symbol, a numeric character, and an upper and lowercase letter.")
     private String newPassword;
 }
