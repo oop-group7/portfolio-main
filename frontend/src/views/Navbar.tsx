eb7c1d8940aed4f9213df22d97eacb91aba60919
0venoven <51714567+0venoven@users.noreply.github.com>
2023-10-06 23:56:11 +0800

    Fixed navbar hamburger expansion
    No explicit styling yet

--- frontend/src/views/Navbar.tsx
+++ frontend/src/views/Navbar.tsx
@@ -1,4 +1,6 @@
 import "bootstrap/dist/css/bootstrap.css";
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+// import { Link, useHistory } from "react-router-dom";
 import logo from "../assets/GoldmanSachsLogo.png";
 import "./css/Navbar.css";
 import { useState, useEffect } from "react";
@@ -37,41 +39,50 @@ function Navbar() {
   }
   return (
     <>
-      <div>
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-          <div className="container-fluid">
-            <img className="logo mx-3" src={logo} />
-            <div id="#isLoginDisplay">
-              <a href="#" className="formstyles" onClick={handleLogout}>
-                {(userData == null) ? "" : "Logout" }
-              </a>
-            </div>
-            <button
-              className="navbar-toggler"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#navbarSupportedContent"
-              aria-controls="navbarSupportedContent"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div
-              className="collapse navbar-collapse"
-              id="navbarSupportedContent"
-            >
-              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                <li className="nav-item">
-                  {/* <router-link class="nav-link" to="/register"
-                                >Register</router-link
-                            > */}
-                </li>
-              </ul>
-            </div>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+          <img className="logo mx-3" src={logo} />
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div
+            className="collapse navbar-collapse"
+            id="navbarSupportedContent"
+          >
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="/homepage">Home</a>
+                {/* <router-link class="nav-link" to="/register"
+                              >Register</router-link> */}
+              </li>
+              <li className="nav-item">
+                <a className="nav-link active" href="/profile">Profile</a>
+              </li>
+              <li className="nav-item" id="#isLoginDisplay">
+                {/* <a href="#" className="formstyles" onClick={handleLogout}>
+                  {(userData == null) ? "" : "Logout" }
+                </a> */}
+                {/* <a className="nav-link active" href="#" onClick={handleLogout}>
+                  {(userData == null) ? "" : "Logout"}
+                </a> */}
+                {userData ? (
+                  <a className="nav-link active" href="#" onClick={handleLogout}>
+                    Logout
+                  </a>
+                ) : null}
+              </li>
+            </ul>
           </div>
-        </nav>
-      </div>
+        </div>
+      </nav>
     </>
   );
 }
