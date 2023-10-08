2a950e54ce63264a4bfca3dcb47002948a238b9e
Darrell Ng <darrellnggg@gmail.com>
2023-10-08 22:02:39 +0800

    WIP homepage

--- frontend/src/views/HomePage.tsx
+++ frontend/src/views/HomePage.tsx
@@ -1,12 +1,48 @@
+import { Grid, Table, TableContainer, TableHead, TableRow, Typography} from "@mui/material";
+import TableCell, { tableCellClasses } from "@mui/material/TableCell";
+import Portfolios from "./components/HompageGrid";
+
 function HomePage() {
 
-
   return (
-    <>
-      <div>
-        <h3>Welcome</h3>
-      </div>  
-    </>
+    <Grid container flexDirection={"column"}>
+      <Grid item color={"white"} mb={2}>
+        <Typography variant="h5">Portfolio Overview</Typography>
+      </Grid>
+      <Grid item sx={{ borderRadius: "5px", backgroundColor: "white", marginBottom: "2rem" }} >
+        <TableContainer>
+          <Table sx={{
+            [`& .${tableCellClasses.root}`]: {
+              borderBottom: "none",
+              textAlign: "center"
+            }
+          }}>
+            <TableHead>
+              <TableRow>
+                <TableCell sx={{borderRight: "solid 1px grey"}}>
+                  <Typography variant="h6" fontWeight={"bold"}>Total</Typography>
+                </TableCell>
+                <TableCell sx={{borderRight: "solid 1px grey"}}>
+                  <Typography fontWeight={"bold"}>Costs</Typography>
+                  <Typography variant="h6" fontWeight={"bold"}>$100,000,000</Typography>
+                </TableCell>
+                <TableCell sx={{borderRight: "solid 1px grey"}}>
+                  <Typography fontWeight={"bold"}>Market Value</Typography>
+                  <Typography variant="h6" fontWeight={"bold"}>$0</Typography>
+                </TableCell>
+                <TableCell>
+                  <Typography fontWeight={"bold"}>Unrealized Gains/Loss</Typography>
+                  <Typography variant="h6" fontWeight={"bold"} color={"red"}>-$100,000,000</Typography>
+                </TableCell>
+              </TableRow>
+            </TableHead>
+          </Table>
+        </TableContainer>
+      </Grid>
+      <Grid>
+        <Portfolios/>
+      </Grid>
+    </Grid>
   );
 }
 
