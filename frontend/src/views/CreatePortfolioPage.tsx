e7bcbf4eb9fe2b8e031bb29e146fca0ead9e22d0
0venoven <51714567+0venoven@users.noreply.github.com>
2023-10-10 23:50:13 +0800

    Blank CreatePortfolioPage

---
+++ frontend/src/views/CreatePortfolioPage.tsx
@@ -0,0 +1,14 @@
+function CreatePortfolioPage() {
+
+
+    return (
+      <>
+        <div>
+          <h3>Welcome</h3>
+        </div>  
+      </>
+    );
+  }
+  
+  export default CreatePortfolioPage;
+  
