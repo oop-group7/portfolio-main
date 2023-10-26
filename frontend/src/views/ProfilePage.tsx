422a1dd4fee586d90d5b50cf27832e50ffd28cca
robinsjules <111500254+robinsjules@users.noreply.github.com>
2023-10-26 19:57:57 +0800

    edited profile page

--- frontend/src/views/ProfilePage.tsx
+++ frontend/src/views/ProfilePage.tsx
@@ -7,12 +7,11 @@ function ProfilePage() {
   const [lastName, setLastName] = useState("");
   const [userName, setUserName] = useState("");
   const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("
-  ");
+  const [password, setPassword] = useState("");
 
   return (
       <div className="container position-relative">
-        <div className="login shadow-lg p-3 bg-body rounded"> {/*Edit such that all buttons are encapsulated inside this container`*/}
+        <div className="register shadow-lg p-3 bg-body rounded"> {/*Edit such that all buttons are encapsulated inside this container`*/}
           <h1 className="heading">Update Profile</h1>
 
           <div className="m-3">
