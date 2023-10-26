9db73474afa84d375764175d724d31015c9a5cdd
tropicbliss <tropicbliss@protonmail.com>
2023-10-26 20:30:03 +0800

    E

--- src/main/java/net/bestcompany/foliowatch/configs/RedisConfig.java
+++ src/main/java/net/bestcompany/foliowatch/configs/RedisConfig.java
@@ -34,9 +34,7 @@ public class RedisConfig {
     public RedisCacheManager cacheManager() {
         RedisCacheConfiguration cacheConfig = myDefaultCacheConfig(Duration.ofMinutes(10))
                 .disableCachingNullValues();
-
-        return RedisCacheManager.builder(redisConnectionFactory()).cacheDefaults(cacheConfig)
-                .withCacheConfiguration("facts", myDefaultCacheConfig(Duration.ofDays(1))).build();
+        return RedisCacheManager.builder(redisConnectionFactory()).cacheDefaults(cacheConfig).build();
     }
 
     private RedisCacheConfiguration myDefaultCacheConfig(Duration duration) {
