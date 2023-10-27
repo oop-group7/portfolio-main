83b4b52d1a1021ddd1a28a36b2ed8ab747383cf5
Darrell Ng <darrellnggg@gmail.com>
2023-10-27 09:25:21 +0800

    revamped portfolios page to reflect all portfolios better with some analytics

--- frontend/src/views/HomePage.tsx
+++ frontend/src/views/HomePage.tsx
@@ -1,46 +1,24 @@
-import { Grid, Table, TableContainer, TableHead, TableRow, Typography} from "@mui/material";
-import TableCell, { tableCellClasses } from "@mui/material/TableCell";
+import { Box, Grid } from "@mui/material";
 import Portfolios from "./components/HompageGrid";
+import HomePageHeader from "./components/HomePageHeader";
+import { SparkLineChart } from "@mui/x-charts";
 
 function HomePage() {
 
   return (
-    <Grid container flexDirection={"column"}>
-      <Grid item color={"white"} mb={2}>
-        <Typography variant="h5">Portfolio Overview</Typography>
+    <Grid container gap={3}>
+      <Grid item xs={12}>
+        <HomePageHeader />
       </Grid>
-      <Grid item sx={{ borderRadius: "5px", backgroundColor: "white", marginBottom: "2rem" }} >
-        <TableContainer>
-          <Table sx={{
-            [`& .${tableCellClasses.root}`]: {
-              borderBottom: "none",
-              textAlign: "center"
-            }
-          }}>
-            <TableHead>
-              <TableRow>
-                <TableCell sx={{borderRight: "solid 1px grey"}}>
-                  <Typography variant="h6" fontWeight={"bold"}>Total</Typography>
-                </TableCell>
-                <TableCell sx={{borderRight: "solid 1px grey"}}>
-                  <Typography fontWeight={"bold"}>Costs</Typography>
-                  <Typography variant="h6" fontWeight={"bold"}>$100,000,000</Typography>
-                </TableCell>
-                <TableCell sx={{borderRight: "solid 1px grey"}}>
-                  <Typography fontWeight={"bold"}>Market Value</Typography>
-                  <Typography variant="h6" fontWeight={"bold"}>$0</Typography>
-                </TableCell>
-                <TableCell>
-                  <Typography fontWeight={"bold"}>Unrealized Gains/Loss</Typography>
-                  <Typography variant="h6" fontWeight={"bold"} color={"red"}>-$100,000,000</Typography>
-                </TableCell>
-              </TableRow>
-            </TableHead>
-          </Table>
-        </TableContainer>
+      <Grid item xs={7}>
+        <Portfolios />
       </Grid>
-      <Grid item width={"100%"}>
-        <Portfolios/>
+      <Grid item container xs justifyContent={"center"}>
+        <Grid item container justifyContent={"center"}>
+          <Box sx={{ flexGrow: 1 }}>
+            <SparkLineChart data={[1, 4, 2, 5, 7, 2, 4, 6]} height={100} />
+          </Box>
+        </Grid>
       </Grid>
     </Grid>
   );
