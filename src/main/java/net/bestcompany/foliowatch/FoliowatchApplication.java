00283ae57cba41bf390f8a00237f01f390bdb579
tropicbliss <tropicbliss@protonmail.com>
2023-10-28 02:27:05 +0800

    E

--- src/main/java/net/bestcompany/foliowatch/FoliowatchApplication.java
+++ src/main/java/net/bestcompany/foliowatch/FoliowatchApplication.java
@@ -3,9 +3,11 @@ package net.bestcompany.foliowatch;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 @SpringBootApplication
 @EnableCaching
+@EnableMongoAuditing
 public class FoliowatchApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(FoliowatchApplication.class, args);
